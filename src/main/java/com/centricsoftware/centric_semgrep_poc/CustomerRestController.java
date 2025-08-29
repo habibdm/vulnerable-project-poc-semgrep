06da9a123e3c335ff26ebdb7b7a425aacb8cc005
Masud Habib <masud.habib@centricsoftware.com>
2025-08-29 09:20:54 -0700

    Adding more vulnearable code

--- src/main/java/com/centricsoftware/centric_semgrep_poc/CustomerRestController.java
+++ src/main/java/com/centricsoftware/centric_semgrep_poc/CustomerRestController.java
@@ -3,7 +3,9 @@ package com.centricsoftware.centric_semgrep_poc;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +20,7 @@ public class CustomerRestController {
     @Inject
     private CustomerService customerService;
 
-    @GetMapping
+    @GetMapping("/vulnerable-xss")
     public String greet(@RequestParam(value = "name", required = false, defaultValue = "John Doe") String name) {
 	return "{\"greetings\": \"Hello! " + name + "\"}";
     }
@@ -28,9 +30,21 @@ public class CustomerRestController {
 	return customerService.findByLastName(lastName);
     }
 
+    @GetMapping("/{id}/vulnerable-csrf-stored-xss")
+    public Customer greet(@PathVariable Long id,
+	    @RequestParam(value = "lastName", required = false, defaultValue = "John Doe") String lastName) {
+
+	return customerService.patchCustomerLastName(id, lastName);
+    }
+
     @PostMapping
     public Customer addANewCustomer(@RequestBody Customer customer) {
 	return customerService.save(customer);
+    }
+
+    @PutMapping
+    public Customer updateCustomer(@RequestBody Customer customer) {
+	return customerService.update(customer);
     }
 
 }
--- src/main/java/com/centricsoftware/centric_semgrep_poc/CustomerService.java
+++ src/main/java/com/centricsoftware/centric_semgrep_poc/CustomerService.java
@@ -1,6 +1,7 @@
 package com.centricsoftware.centric_semgrep_poc;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,25 @@ public class CustomerService {
 
     public Customer save(Customer customer) {
 	return customerRepository.saveAndFlush(customer);
+    }
+
+    public Customer update(Customer customer) {
+	boolean doesCustomerExist = customerRepository.existsById(customer.getId());
+	if (doesCustomerExist) {
+	    return customerRepository.saveAndFlush(customer);
+	}
+
+	return null;
+
+    }
+
+    public Customer patchCustomerLastName(Long customerId, String lastName) {
+	Optional<Customer> existingCustomer = customerRepository.findById(customerId);
+	return existingCustomer.map(customer -> {
+	    customer.setLastName(lastName);
+	    return customerRepository.save(customer);
+	}).orElse(null);
+
     }
 
 }
